16ae77363906bd29ad737a4dc9c3deff5d0c1b5b
Rasa <therain@rasas-air.home>
2019-04-24 17:13:24 -0400

    BarkingDog

--- src/Main.java
+++ src/Main.java
@@ -5,8 +5,14 @@ public class Main {
 //        System.out.println("Miles = " + miles);
 //        SpeedConverter.printConversion(10.5);
 
-        MegaBytesConverter.printMegaBytesAndKiloBytes(2500);
-        MegaBytesConverter.printMegaBytesAndKiloBytes(-1024);
-        MegaBytesConverter.printMegaBytesAndKiloBytes(5000);
+//        MegaBytesConverter.printMegaBytesAndKiloBytes(2500);
+//        MegaBytesConverter.printMegaBytesAndKiloBytes(-1024);
+//        MegaBytesConverter.printMegaBytesAndKiloBytes(5000);
+
+        BarkingDog.shouldWakeUp(true,1);
+        boolean test = BarkingDog.shouldWakeUp(false,2);
+        System.out.println(test);
+        BarkingDog.shouldWakeUp(true,8);
+        BarkingDog.shouldWakeUp(true,-1);
     }
     }
