3906828af84f395ae524585e24d5369d53651315
Rasa <therain@rasas-air.home>
2019-04-26 15:18:32 -0400

    LeapYear

---
+++ src/LeapYear.java
@@ -0,0 +1,12 @@
+public class LeapYear {
+
+    public static boolean isLeapYear(int year){
+        if(year >= 1 && year <= 9999) {
+            if(((year%4) == 0 && (year%100 != 0))|| ((year%100) == 0) && ((year%400) == 0)){
+                return true;
+            } return false;
+        } else {return false;}
+    }
+}
+
+
--- src/Main.java
+++ src/Main.java
@@ -9,10 +9,23 @@ public class Main {
 //        MegaBytesConverter.printMegaBytesAndKiloBytes(-1024);
 //        MegaBytesConverter.printMegaBytesAndKiloBytes(5000);
 
-        BarkingDog.shouldWakeUp(true,1);
-        boolean test = BarkingDog.shouldWakeUp(false,2);
-        System.out.println(test);
-        BarkingDog.shouldWakeUp(true,8);
-        BarkingDog.shouldWakeUp(true,-1);
+//        BarkingDog.shouldWakeUp(true,1);
+//        boolean test = BarkingDog.shouldWakeUp(false,2);
+//        System.out.println(test);
+//        BarkingDog.shouldWakeUp(true,8);
+//        BarkingDog.shouldWakeUp(true,-1);
+
+        System.out.println(LeapYear.isLeapYear(1700));
+        System.out.println(LeapYear.isLeapYear(1800));
+        System.out.println(LeapYear.isLeapYear(1900));
+        System.out.println(LeapYear.isLeapYear(2100));
+        System.out.println(LeapYear.isLeapYear(2200));
+        System.out.println(LeapYear.isLeapYear(2300));
+        System.out.println(LeapYear.isLeapYear(2500));
+        System.out.println(LeapYear.isLeapYear(2600));
+
+        System.out.println(LeapYear.isLeapYear(1600));
+        System.out.println(LeapYear.isLeapYear(2000));
+        System.out.println(LeapYear.isLeapYear(2400));
     }
     }
